345bce795641b86ec41638d20dae6d3b08b8f253
traficmaniac-lgtm <traficmaniac@gmail.com>
2026-01-29 13:33:59 +0200

    update

--- ps.bat
+++ ps.bat
@@ -1,53 +1,10 @@
 @echo off
-chcp 65001 >nul
-setlocal enabledelayedexpansion
+cd /d "%~dp0"
 
-set "ROOT=C:\Users\Юрий\Desktop\HDG"
-set "LOG=%ROOT%\HDG_PUSH.log"
+git update-index --skip-worktree config/settings.json >nul 2>&1
 
-(
-  echo === START %date% %time% ===
-  echo ROOT=%ROOT%
-  echo.
+git add -A
+git commit -m "update" >nul 2>&1
+git push origin main
 
-  if not exist "%ROOT%\" (
-    echo [ERR] Folder not found: "%ROOT%"
-    goto :end
-  )
-
-  cd /d "%ROOT%" || (echo [ERR] cd failed & goto :end)
-
-  echo --- where git ---
-  where git
-  echo.
-
-  echo --- git --version ---
-  git --version
-  echo.
-
-  echo --- git status ---
-  git status
-  echo.
-
-  echo --- git add -A ---
-  git add -A
-  echo.
-
-  echo --- git commit -m "update" ---
-  git commit -m "update"
-  echo.
-
-  echo --- git push origin main ---
-  git push origin main
-  echo.
-
-  echo [OK] Done
-) > "%LOG%" 2>&1
-
-:end
-echo.
-echo Log saved to: "%LOG%"
-type "%LOG%"
-echo.
 pause
-
